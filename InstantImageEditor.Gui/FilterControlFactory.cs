ce19a552eb17cebb827e4a7c5f4830861a72fae7
andrew_denisov <andrew_denisov@bitbucket.net>
2015-03-17 21:33:12 +0600

    Lesson_2

---
+++ InstantImageEditor.Gui/FilterControlFactory.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace InstantImageEditor.Gui
+{
+	public static class FilterControlFactory
+	{
+		public static FilterControlBase GetFilterControl(string parameter)
+		{
+			switch (parameter)
+			{
+				case "Resize":
+					return new ResizeControl();
+				default:
+					throw new ArgumentException(parameter);
+			}
+		}
+	}
+}
